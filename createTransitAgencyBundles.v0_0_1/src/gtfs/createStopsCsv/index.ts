ee9c9c41dc5d0986ff819bd0418fc9e605cbfe38
Paul Tomchik <pauljtomchik@gmail.com>
2022-08-18 21:59:31 -0400

    Added create_route_stops_csv command

--- createTransitAgencyBundles.v0_0_1/src/gtfs/createStopsCsv/index.ts
+++ createTransitAgencyBundles.v0_0_1/src/gtfs/createStopsCsv/index.ts
@@ -1,5 +1,5 @@
 import { mkdirSync, createWriteStream } from "fs";
-import { join, basename } from "path";
+import { join, dirname, basename } from "path";
 
 import Database from "better-sqlite3";
 import * as csv from "fast-csv";
@@ -9,7 +9,62 @@ export type DumpStopsParams = {
   stopsCsvDir: string;
 };
 
-export default async function main({
+export type RouteStopsParams = {
+  gtfsAgencySqlitePath: string;
+  routeId: string;
+  routeStopsCsvPath: string;
+};
+
+export async function dumpRouteStopsForAgency(params: RouteStopsParams) {
+  const { gtfsAgencySqlitePath, routeId, routeStopsCsvPath } = params;
+
+  const outDirPath = dirname(routeStopsCsvPath);
+  mkdirSync(outDirPath, { recursive: true });
+
+  const ws = createWriteStream(routeStopsCsvPath);
+
+  const stream = csv.format({ headers: ["stop_id", "stop_lat", "stop_lon"] });
+
+  stream.pipe(ws);
+
+  const q = `
+    SELECT DISTINCT
+        stop_id,
+        stop_lat,
+        stop_lon
+      FROM stops
+        INNER JOIN stop_times
+          USING (stop_id)
+        INNER JOIN trips
+          USING (trip_id)
+      WHERE ( route_id = ? )
+      ORDER BY stop_id
+  `;
+
+  const agencyName = basename(gtfsAgencySqlitePath, ".sqlite3");
+
+  const db = new Database(gtfsAgencySqlitePath);
+
+  const iter = db.prepare(q).iterate([routeId]);
+
+  for (const { stop_id, stop_lat, stop_lon } of iter) {
+    if (/::/.test(stop_id)) {
+      throw new Error("We need another delimiter for agency_name/stop_id");
+    }
+
+    stream.write({
+      stop_id: `${agencyName}::${stop_id}`,
+      stop_lat,
+      stop_lon,
+    });
+  }
+
+  stream.end();
+
+  return params;
+}
+
+export async function dumpAllStopsAllAgencies({
   gtfsAgencySqlitePaths,
   stopsCsvDir,
 }: DumpStopsParams) {
--- createTransitAgencyBundles.v0_0_1/src/gtfs/createStopsCsv/yargs_wrapper.ts
+++ createTransitAgencyBundles.v0_0_1/src/gtfs/createStopsCsv/yargs_wrapper.ts
@@ -1,29 +1,59 @@
 import { relative } from "path";
 
-import main, { DumpStopsParams } from ".";
+import {
+  dumpAllStopsAllAgencies,
+  DumpStopsParams,
+  dumpRouteStopsForAgency,
+  RouteStopsParams,
+} from ".";
 
-const builder = {
-  gtfsAgencySqlitePaths: {
-    desc: "Path to the GTFS SQLite Databases",
-    demand: true,
-    type: "array",
+export const createAllStopsCsv = {
+  desc: "Dump all stops in all specified gtfsAgencySqlitePaths to an all_stops.csv file in the specified stopsCsvDir.",
+  command: "create_all_stops_csv",
+  builder: {
+    gtfsAgencySqlitePaths: {
+      desc: "Paths to the GTFS SQLite Databases",
+      demand: true,
+      type: "array",
+    },
+    stopsCsvDir: {
+      desc: "Stops CSV directory",
+      demand: true,
+      type: "string",
+    },
   },
-  stopsCsvDir: {
-    desc: "Stops CSV directory",
-    demand: true,
-    type: "string",
-  },
-};
-
-export const createStopsCsv = {
-  desc: "Load the specified GTFS feed into SQLite.",
-  command: "load_gtfs_feed_into_sqlite",
-  builder,
   async handler(argv: DumpStopsParams) {
-    const { allStopsCsvPath } = await main(argv);
+    const { allStopsCsvPath } = await dumpAllStopsAllAgencies(argv);
 
     const relPath = relative(process.cwd(), allStopsCsvPath);
 
     console.log("SQLite DB written to", relPath);
+  },
+};
+
+export const createRouteStopsCsv = {
+  desc: "Dump all stops for the specified routeId in the specified gtfsAgencySqlitePath DB to routeStopsCsvPath.",
+  command: "create_route_stops_csv",
+  builder: {
+    gtfsAgencySqlitePath: {
+      desc: "Path to the GTFS SQLite Database",
+      demand: true,
+      type: "string",
+    },
+    routeId: {
+      desc: "The routeId identifyig the route whose stops to dump.to the GTFS SQLite Database",
+      demand: true,
+      type: "string",
+    },
+    routeStopsCsvPath: {
+      desc: "Stops CSV directory",
+      demand: true,
+      type: "string",
+    },
+  },
+  async handler(argv: RouteStopsParams) {
+    const result = await dumpRouteStopsForAgency(argv);
+
+    console.log(JSON.stringify(result, null, 4));
   },
 };
