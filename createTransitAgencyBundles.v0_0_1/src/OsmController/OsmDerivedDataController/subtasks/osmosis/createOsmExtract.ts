a624cb7e4b3c0843e4dce0b10f61025f399e68d0
Paul Tomchik <pauljtomchik@gmail.com>
2022-08-25 11:07:17 -0400

    Download osmosis if not in lib/

--- createTransitAgencyBundles.v0_0_1/src/OsmController/OsmDerivedDataController/subtasks/osmosis/createOsmExtract.ts
+++ createTransitAgencyBundles.v0_0_1/src/OsmController/OsmDerivedDataController/subtasks/osmosis/createOsmExtract.ts
@@ -11,6 +11,7 @@ import {
   writeFile as writeFileAsync,
 } from "fs/promises";
 import { join, basename } from "path";
+import { existsSync } from "fs";
 import { execSync } from "child_process";
 
 import * as turf from "@turf/turf";
@@ -18,16 +19,22 @@ import tmp from "tmp";
 
 import { tmpDirPath, osmosisExecutablePath } from "../../constants";
 
+import downloadOsmosisToLibrary from "./downloadOsmosisToLibrary";
+
 export function getOsmosisExtractFilter(
   geojsonPoly: turf.Feature<turf.Polygon | turf.MultiPolygon>,
   extractName: string
 ) {
   if (geojsonPoly.geometry.type === "Polygon") {
+    // @ts-ignore
     geojsonPoly.geometry.type = "MultiPolygon";
+    // @ts-ignore
     geojsonPoly.geometry.coordinates = [geojsonPoly.geometry.coordinates];
   }
 
-  const sections = geojsonPoly?.geometry.coordinates
+  const sections = (<turf.Feature<turf.MultiPolygon>>(
+    geojsonPoly
+  ))?.geometry.coordinates
     .map((ring, i) => {
       const polygon = turf.polygon([ring[0]]);
       const buffered = turf.buffer(polygon, 0.001, {
@@ -78,6 +85,10 @@ export default async function createOsmExtract({
   // @ts-ignore
   await writeFileAsync(tmpFile.name, poly);
 
+  if (!existsSync(osmosisExecutablePath)) {
+    await downloadOsmosisToLibrary();
+  }
+
   const command = `${osmosisExecutablePath} \
       --read-pbf-fast file=${sourceOsmFilePath} \
       --sort type="TypeThenId" \
@@ -87,7 +98,7 @@ export default async function createOsmExtract({
       --write-pbf ${extractOsmFilePath}
     `;
 
-  console.log(command);
+  // console.log(command);
 
   // Because we allow relative paths for the files, we MUST inherit CWD.
   execSync(command, { cwd: process.cwd() });
