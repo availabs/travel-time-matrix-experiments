a098be62b345dd8b9058af5ac8680ffc85f4055a
Paul Tomchik <pauljtomchik@gmail.com>
2022-08-25 17:11:26 -0400

    Removed console.logging

--- createTransitAgencyBundles.v0_0_1/src/OsmController/tasks/add_osm_pbf.ts
+++ createTransitAgencyBundles.v0_0_1/src/OsmController/tasks/add_osm_pbf.ts
@@ -1,6 +1,5 @@
 import BaseDataControl, { AddOsmPbfFileParams } from "../OsmBaseDataController";
 
 export default async function addOsmPbfToBaseData(params: AddOsmPbfFileParams) {
-  console.log(JSON.stringify(params, null, 4));
   return await BaseDataControl.addOsmPbfFile(params);
 }
